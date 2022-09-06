3d8db3d45107598874d38bd7712248d2c1eab385
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2022-09-06 13:33:34 +0000

    Revert "revert"
    This reverts commit f136ebc4a808739d8a41fb3dff0367f609a0f3a5.

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 def hello_world():
     """Prentar hello world á root"""
     return "Hello, World!"
-#
+
+@app.route("/blog")
+def blog():
+    return "blog"
+
+    
 @app.route("/blog")
 def blog():
     return "blog"
